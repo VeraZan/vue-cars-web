c099a4e41b8edaad96b9392eb8458ef952eda263
yfzan <yfzan@clustertech.com>
2020-10-20 16:14:19 +0800

    会员管理和安全界面

---
+++ src/views/safe/password.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="user-container">
+    <Back column="修改登录密码" />
+    <div class="cars-form-ui">
+      <el-form ref="form" :model="form">
+        <el-form-item>
+          <el-input v-model="form.password" type="password" placeholder="原始密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.newPassword" type="password" placeholder="新密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.confirmPassword" type="password" placeholder="确认密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <button type="button" class="button-vcode">获取验证码</button>
+          <el-input v-model="form.code" placeholder="验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" class="button-block">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Password",
+  //components: {},
+  data(){
+    return {
+      form: {
+        password: '',
+        newPassword: '',
+        confirmPassword: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    }
+  }
+}
+</script>
+<style lang="scss"></style>
