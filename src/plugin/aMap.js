91f1b26e8fb80ae10d7b89700ccc709450700e6b
yfzan <yfzan@clustertech.com>
2020-10-28 17:56:37 +0800

    地图定位，停车场打点

--- src/plugin/aMap.js
+++ src/plugin/aMap.js
@@ -7,7 +7,7 @@ VueAMap.initAMapApiLoader({
   // 高德key
   key: '9a10436d6258e1f38f8fd8b8f0c0f2a1', // 自己到官网申请
   // 插件集合 （插件按需引入）
-  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor', 'AMap.MarkerClusterer'],
+  plugin: ['AMap.Geolocation'],
   v: '1.4.15', // 我也不知道为什么要写这个，不写项目会报错，而且我随便写的，跟我下载的版本对应不了
   uiVersion: '1.0.11' // ui版本号，也是需要写
 })
--- src/views/amap/index.vue
+++ src/views/amap/index.vue
@@ -1,27 +1,102 @@
 <template>
   <div class="amap-wrap">
-    <el-amap vid="amapContainer" class="amap-demo"></el-amap>
+    <el-amap ref="map" vid="amapContainer" :amap-manager="amapManager" :center="center" :zoom="zoom" :events="events" class="amap-demo">
+      <!--覆盖物 - 圆-->
+      <el-amap-circle 
+        v-for="item in circle" 
+        :key="item.id" 
+        :center="item.center" 
+        :radius="item.radius" 
+        :fillColor="item.color" 
+        :strokeColor="item.color" 
+        :strokeOpacity="item.strokeOpacity" 
+        :strokeWeight="item.strokeWeight"
+      ></el-amap-circle>
+      <!--覆盖物 - 停车场-->
+      <el-amap-marker 
+        v-for="(item, index) in parking" 
+        :key="item.id" 
+        :content="item.content" 
+        :offset="item.offset" 
+        :position="item.position" 
+        :vid="index"
+      ></el-amap-marker>   
+      <!--覆盖物 - 停车场 - 车辆-->
+      <el-amap-marker 
+        v-for="(item, index) in parking" 
+        :extData="item" 
+        :events="item.events" 
+        :key="item.id + index" 
+        :content="item.text" 
+        :offset="item.offsetText" 
+        :position="item.position" 
+        :vid="index"
+      ></el-amap-marker>                   
+    </el-amap>
   </div>
 </template>
 
 <script>
 import { AMapManager,lazyAMapApiLoaderInstance } from 'vue-amap';
+import { SelfLocation } from "./location";
 export default {
   name:"Map",
+  props: {
+    parking: {
+      type: Array,
+      default: () => []
+    }
+  },
   data(){
+    const _this = this;
     return{
-      map:null,
-      zoom:18,
-      events:{}
+      map: null,
+      zoom: 18,
+      amapManager: new AMapManager(),
+      center: [121.59996, 31.197646],
+      events: {
+        init: (o) => {
+          lazyAMapApiLoaderInstance.load().then(() => {
+            _this.initMap();
+          })
+        }
+      },
+      // 自身定位
+      circle: [],
+    }
+  },
+  methods:{
+    initMap(){
+      this.map = this.amapManager.getMap();
+      // 地图初始化完成回调
+      this.$emit("callback", {
+        function: "loadMap"
+      })
+      // 自身定位
+      this.selfLocation();
+    },
+    selfLocation(){
+      SelfLocation({
+        map: this.map,
+        complete: (val) => this.selfLocationComplete(val)
+      })
+    },
+    /** 自身定位 成功回调 */
+    selfLocationComplete(data){
+      this.self_lng = data.position.lng;
+      this.self_lat = data.position.lat;
+      const json = {
+        radius: 4,
+        color: "#393e43",
+        strokeOpacity: "0.2",
+        strokeWeight: "30"
+      }
+      json.center = [this.self_lng, this.self_lat];
+      this.circle.push(json);
     }
   },
   mounted(){
-    lazyAMapApiLoaderInstance.load().then(() => {
-      this.map = new AMap.Map('amapContainer', {
-        zoom:this.zoom,//级别
-        center: [116.397428, 39.90923],//中心点坐标
-      });
-    })
+   
   }
 }
 </script>
